236ee5fd5d7fd5c5379b88c4bbd53462e62b58a8
jonathan.schroder <jonathan.schroder@00544a2a-6920-0410-b3ff-a9a6ad1c33a1>
2006-10-31 22:01:38 +0000

    le projet n'est plus compilable parce que je n'ai pas acheve mon travail sur main.c. Ajout d'un pointeur sur structure t_tout et début de remplissage du champ boutons... Documentation de main.h. Ecriture du corps de la fonction "clicked" et modification de la config des callbacks des boutons.

--- agneauetloups/main.c
+++ agneauetloups/main.c
@@ -1,12 +1,21 @@
 #include "projet.h"
 
+/// fonction d'initialisation du programme
+/**
+crée les widgets, configure leurs comportement et lance l'affichage
+*/
+
 int main (int argc, char *argv[])
 {
+
   GtkWidget *win = NULL;
   GtkWidget *button[50];
   GtkWidget *table;
   t_movement move;
   gint i;
+
+  t_tout* tout;
+
   /* Initialize GTK+ */
   gtk_init(&argc,&argv);
   win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
@@ -19,21 +28,42 @@ int main (int argc, char *argv[])
   gtk_container_add(GTK_CONTAINER(win),GTK_WIDGET(table));
   
   move.lastone = NULL;
+
   
+  //mise des labels des boutons à VIDE
+  //positionnement des boutons
   for(i=0;i<50;i++)
   {
       button[i] = gtk_button_new_with_label("");
       gtk_table_attach_defaults(GTK_TABLE(table),button[i],2*(i%5)+1-(i%10-i%5)/5,2*(i%5)+1-(i%10-i%5)/5+1,2*(i-i%10)/10+(i%10-i%5)/5,2*(i-i%10)/10+(i%10-i%5)/5+1);
   }
-  
-  for(i=0;i<5;i++) gtk_button_set_label(GTK_BUTTON(button[i]),"L");
+  //creation de la structure tout
+  tout = (t_tout*)malloc(sizeof(t_tout));
+  //champs de data pour chacun des pions (allocation du pointeur sur tableau)
+  tout->
+  for(i=0;i<5;i++)
+  {
+    //mise des labels pour les boutons des pions
+    gtk_button_set_label(GTK_BUTTON(button[i]),"L");
+    //creation des champs de data pour chacun des pions
+    
   gtk_button_set_label(GTK_BUTTON(button[47]),"A");
   
-  for(i=0;i<50;i++) g_signal_connect(G_OBJECT(button[i]),"clicked",G_CALLBACK(make_movement),&move);
+  //configuration de la fonction callback a appeler quand un bouton est presse
+  for(i=0;i<50;i++) //g_signal_connect(G_OBJECT(button[i]),"clicked",G_CALLBACK(make_movement),&move);
+	g_signal_connect(G_OBJECT(button[i]),"clicked",G_CALLBACK(make_movement),tout);
   
+  //insertion du tableau de boutons dans la pointeur sur structure "tout"
+  tout->button = button;
+  
+
   gtk_widget_show_all(win);
   gtk_main ();
   return 0;
+}
+
+void clicked(GtkWidget *emitter, t_tout* tout)
+{
 }
 
 void make_movement(GtkWidget *newone,t_movement *move)
--- agneauetloups/main.h
+++ agneauetloups/main.h
@@ -16,35 +16,74 @@ typedef struct {
 
 void make_movement(GtkWidget *newone,t_movement *move);
 
-typedef struct {int time;
+void clicked(GtkWidget *emitter, t_tout* tout);
+
+/// structure regroupant les informations necessaires a l'affichage d'une animation
+typedef struct {
+		/// date actuelle en millisecondes dans l'animation
+		int time;
+		/// duree en millisecondes de l'animation
 		int timemax;
+		/// index de l'image acutellement affichee de l'animation
 		int index;
+		/// plus haut index possible pour une image de l'animation
 		int indexmax;
+		/// tableau de pointeurs sur les images chargees en memoire
 		GdkPixbuf **images;
 		}t_animation;
-		
-typedef struct {int position;
+
+/// structure decrivant l'etat d'un agneau
+typedef struct {/// index de la case ou est situe l'agneau
+		int position;
+		/// tableau regroupant les 4 positions autorisees au tour suivant
 		int poss[4];
 		}t_lamb;
 
-typedef struct {int position;
+/// structure decrivant l'etat d'un loup
+typedef struct {/// index de la case ou est situe le loup
+		int position;
+		/// tableau regroupant les deux positions autorisees au tour suivant
 		int poss[2];
 		}t_wolf;
-	
-typedef struct {t_lamb lamb;
+
+/// structure regroupant les informations des loups et de l'agneau a un instant du jeu
+/**
+* cette structure est redefinie en memoire a chaque tour. Elle est utilisee lors de la sauvegarde de la partie
+* ainsi que lors du rechargement d'une partie sauvegardee.
+*/
+typedef struct {/// position et deplacements possibles de l'agneau
+		t_lamb lamb;
+		/// position et deplacements possibles des cinq loups
 		t_wolf wolf[5];
+		/// index du joueur devant jouer
 		int cur_player;
+		/// informations sur les deux joueurs : pour chacun s'il est gere par un utilisateur ou par l'ordinateur
 		int is_human[2];
 		}t_data;
 
-typedef struct {int skin;
-		       GtkWidget *button[50];
-		       GdkPixbuf **images;
-		       t_animation *animation;
-		       GtkWidget *statusbar;
-		       t_data *data;
-		       GList *history;
-		       }t_tout;
+/// structure groupant les donnees necessaire aux fonctionnement du programme
+/** 
+* l'unique instance de t_tout est pointee par un pointeur appele "tout".
+* Cette structure regroupe les pointeurs vers les widgets de la fenetre, les graphiques charges en memoire, l'historique de la partie,
+* ainsi que les donnees concernant la situation des pions des differents joueurs a l'instant present
+*/
+
+typedef struct {
+	/// index du skin en utilisation
+	int skin;
+	/// tableau de pointeurs sur les boutons
+	GtkWidget *button[50];
+	/// tableau de pointeurs sur les images fixes
+	GdkPixbuf **images;
+	/// tableau de pointeurs sur animations
+	t_animation *animation;
+	/// pointeur sur la barre de status
+	GtkWidget *statusbar;
+	/// pointeur sur les donnees du jeu a cet instant
+	t_data *data;
+	/// ancre de la liste chainee d'historique de la partie
+	GList *history;
+	}t_tout;
 
 
 			   
