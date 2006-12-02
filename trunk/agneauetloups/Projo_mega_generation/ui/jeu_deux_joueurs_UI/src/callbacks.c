6865036e13bad842abe24c52af1ff99e116054f7
jonathan.schroder <jonathan.schroder@00544a2a-6920-0410-b3ff-a9a6ad1c33a1>
2006-12-02 20:34:52 +0000

    la config de la partie deux joueurs locale marche

--- trunk/agneauetloups/Projo_mega_generation/ui/jeu_deux_joueurs_UI/src/callbacks.c
+++ trunk/agneauetloups/Projo_mega_generation/ui/jeu_deux_joueurs_UI/src/callbacks.c
@@ -8,3 +8,35 @@
 #include "interface.h"
 
 
+
+void
+on_agneau_radio_joueur_toggled         (gpointer         user_data,
+                                        GtkToggleButton *togglebutton)
+{
+gtk_widget_set_sensitive((GtkWidget*)user_data,gtk_toggle_button_get_active(togglebutton) ? TRUE : FALSE);
+}
+
+
+void
+on_agneau_radio_ordi_toggled           (gpointer         user_data,
+                                        GtkToggleButton *togglebutton)
+{
+gtk_widget_set_sensitive((GtkWidget*)user_data,gtk_toggle_button_get_active(togglebutton) ? FALSE : TRUE);
+}
+
+
+void
+on_loups_radio_joueur_toggled          (gpointer         user_data,
+                                        GtkToggleButton *togglebutton)
+{
+gtk_widget_set_sensitive((GtkWidget*)user_data,gtk_toggle_button_get_active(togglebutton) ? TRUE : FALSE);
+}
+
+
+void
+on_loups_radio_ordi_toggled            (gpointer         user_data,
+                                        GtkToggleButton *togglebutton)
+{
+gtk_widget_set_sensitive((GtkWidget*)user_data,gtk_toggle_button_get_active(togglebutton) ? FALSE : TRUE);
+}
+
