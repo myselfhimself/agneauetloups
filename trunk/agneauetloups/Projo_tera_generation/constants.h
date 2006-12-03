8b82368b0acd3daddfdea53e8b2111ed1cc49755
st.regnier <st.regnier@00544a2a-6920-0410-b3ff-a9a6ad1c33a1>
2006-12-03 20:19:48 +0000

    debug theme

--- trunk/agneauetloups/Projo_tera_generation/constants.h
+++ trunk/agneauetloups/Projo_tera_generation/constants.h
@@ -52,13 +52,6 @@ char PATH[128];
 #define OPERATING_SYSTEM LINUX
 #endif
 
-#define GLADE_HOOKUP_OBJECT(component,widget,name) \
-  g_object_set_data_full (G_OBJECT (component), name, \
-    gtk_widget_ref (widget), (GDestroyNotify) gtk_widget_unref)
-
-#define GLADE_HOOKUP_OBJECT_NO_REF(component,widget,name) \
-  g_object_set_data (G_OBJECT (component), name, widget)
-  
 #include "structures.h"
   
 GtkWidget *GTK_WINTHEME;
